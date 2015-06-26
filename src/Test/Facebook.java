7b68971f6df05daad496d44e22ba6af37c968b80
INoTa <Inoue_Kenta@hotmail.com>
2015-06-26 17:21:43 +0700

    push restfb

---
+++ src/Test/Facebook.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Untitled25364
+ */
+public class Facebook {
+    public static void main(String[] args) throws IOException{
+
+    }
+}
